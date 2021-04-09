227e982400c1b13b516f8cdf2021020e003534ec
natehans <79099673+natehans@users.noreply.github.com>
2021-04-09 13:47:31 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,11 +1,17 @@
 import unittest
 
-from sumFunction import mySum
+import sumFunction*
 
 
 class TestSum(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(mySum(1, 2), 3, "Should be three")
+    def test_divide(unittest.TestCase):
+        self.assertEqual(divide(4,2),2,"Should be 2")
+    def test_list(unittest.TestCase):
+        myList=[1,2,3,4,5]
+        self.assertEqual(secondItemInList(myList),2,"Should be 2")
+     
 
 
 if __name__ == '__main__':
